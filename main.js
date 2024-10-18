1bdde2709a0b14848c0c741bee9b894e3e3c5ce2
Shourov Foisal <shourovfoisal@gmail.com>
2024-10-18 23:14:29 +0600

    designed the card

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-(function () {})();
-
 async function fetchBooks() {
   // const response = await fetch("https://gutendex.com/books");
   // console.log("🚀 ~ fetchBooks ~ response:", response);
 }
 
-fetchBooks();
+(function init() {
+  fetchBooks();
+})();
