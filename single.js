678b124bf0b6f2e49691a65cc7be327e97d20ab0
Shourov Foisal <shourovfoisal@gmail.com>
2024-10-20 20:42:55 +0600

    single page render done

--- single.js
+++ single.js
@@ -7,12 +7,58 @@ async function fetchSingleBook() {
   if (selectedBookId !== null) {
     const response = await fetch(`${API_BASE_URL}/books/${selectedBookId}/`);
     const result = await response.json();
-    console.log("🚀 ~ fetchSingleBook ~ result:", result);
 
+    // Setting image
+    const imageParentElement = document.getElementById("single-book-image");
     const bookImageLink = result?.formats?.["image/jpeg"];
+    const newImageElement = document.createElement("img");
+    newImageElement.src = bookImageLink;
+    newImageElement.alt = result?.title;
+    imageParentElement.append(newImageElement);
+
+    // Setting the other data
+
+    const idElement = document.getElementById("single-book-id");
+    idElement.innerHTML = `#${result?.id}`;
+
+    const titleElement = document.getElementById("single-book-title");
+    titleElement.innerHTML = result?.title;
+
+    const authorElement = document.getElementById("single-book-author");
+    result?.authors?.forEach((eachAuthor) => {
+      const newDivElement = document.createElement("div");
+      newDivElement.append(eachAuthor?.name);
+      const newSpanElement = document.createElement("span");
+      newSpanElement.innerHTML = `[${eachAuthor?.birth_year} - ${eachAuthor?.death_year}]`;
+      newDivElement.append(newSpanElement);
+      authorElement.append(newDivElement);
+    });
+
+    const genresWrapperElement = document.getElementById(
+      "single-book-genres-all"
+    );
+    result?.subjects?.forEach((eachSubject) => {
+      const newParagraphElement = document.createElement("p");
+      newParagraphElement.innerHTML = eachSubject;
+      genresWrapperElement.append(newParagraphElement);
+    });
+
+    const bookshelvesWrapperElement = document.getElementById(
+      "single-book-bookshelves-all"
+    );
+    result?.bookshelves?.forEach((eachShelf) => {
+      const newParagraphElement = document.createElement("p");
+      newParagraphElement.innerHTML = eachShelf;
+      bookshelvesWrapperElement.append(newParagraphElement);
+    });
+
+    const readThisBookLinkElement = document.getElementById(
+      "single-book-read-link"
+    );
+    readThisBookLinkElement.href = result?.formats?.["text/html"];
   }
 }
 
 (function init() {
-  // fetchSingleBook();
+  fetchSingleBook();
 })();
